4c8d9649e900d8bda03799be41a9b927d663c6f4
KEWNRU <n0kky21rus@gmail.com>
2023-04-01 14:48:03 +0300

    3. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.chess.firuges.black;
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BishopBlackTest {
+
+    @Test
+    void position() {
+        BishopBlack bp = new BishopBlack(Cell.A6);
+        assertThat(bp.position(), is(Cell.A6));
+    }
+
+    @Test
+    public void Way(){
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(bishopBlack.way(Cell.G5), is(expected));
+    }
+
+    @Test
+    void copy() {
+        BishopBlack bp = new BishopBlack(Cell.A6);
+        assertThat(bp.copy(Cell.A6).position(), is(Cell.A6));
+    }
+}
